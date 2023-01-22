308ea1881dd52731cfc366014910558248208f98
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-01-22 23:16:33 +0530

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,90 +1,61 @@
-const chatForm = document.getElementById('chat-form');
-const chatMessages = document.querySelector('.chat-messages');
-const roomName = document.getElementById('room-name');
-const userList = document.getElementById('users');
-
-// Get username and room from URL
-const { username, room } = Qs.parse(location.search, {
-  ignoreQueryPrefix: true,
-});
+const chatForn = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
+const roomName = document.getElementById("room-name");
+const userList = document.getElementById("users");
 
 const socket = io();
 
-// Join chatroom
-socket.emit('joinRoom', { username, room });
-
-// Get room and users
-socket.on('roomUsers', ({ room, users }) => {
-  outputRoomName(room);
-  outputUsers(users);
+//get username and room from URL
+const {username,room}=Qs.parse(location.search,{
+    ignoreQueryPrefix:true
 });
 
-// Message from server
-socket.on('message', (message) => {
-  console.log(message);
-  outputMessage(message);
+//join chat room
+socket.emit("joinRoom" , {username , room});
 
-  // Scroll down
-  chatMessages.scrollTop = chatMessages.scrollHeight;
+//Get room and users
+socket.on("roomUsers" , ({room , users})=>{
+    outputRoomName(room);
+    outputUsers(users);
 });
 
-// Message submit
-chatForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  // Get message text
-  let msg = e.target.elements.msg.value;
-
-  msg = msg.trim();
-
-  if (!msg) {
-    return false;
-  }
-
-  // Emit message to server
-  socket.emit('chatMessage', msg);
-
-  // Clear input
-  e.target.elements.msg.value = '';
-  e.target.elements.msg.focus();
+//the emiting message(defined in server.js file) will be catched here / message from server
+socket.on("message" , message=>{ 
+    console.log(message);
+    outputMessage(message);
+    //every time when we get a message just scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
-// Output message to DOM
-function outputMessage(message) {
-  const div = document.createElement('div');
-  div.classList.add('message');
-  const p = document.createElement('p');
-  p.classList.add('meta');
-  p.innerText = message.username;
-  p.innerHTML += `<span>${message.time}</span>`;
-  div.appendChild(p);
-  const para = document.createElement('p');
-  para.classList.add('text');
-  para.innerText = message.text;
-  div.appendChild(para);
-  document.querySelector('.chat-messages').appendChild(div);
+//submission of message
+chatForn.addEventListener("submit" , e=>{
+    e.preventDefault(); //when you submit a form it automatically submit to a file and we want to stop that from happening so 
+                        //we use 'e.preventDefault' which will prevent the default behaviour
+    const msg = e.target.elements.msg.value; //fetching the message text here , from where the user is used to submit 
+    socket.emit("chatMessage" , msg);//Emit message to server
+    
+    //clear msg from input bar
+    e.target.elements.msg.value = "";
+    e.target.elements.msg.focus();
+});
+
+//output message to DOM in oreder to do so , do some DOM manipulation here
+function outputMessage(message){
+    const div = document.createElement("div");
+    div.classList.add("message");
+    div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+    <p class="text">
+        ${message.text}
+    </p>`;
+    document.querySelector(".chat-messages").appendChild(div);
 }
 
-// Add room name to DOM
-function outputRoomName(room) {
-  roomName.innerText = room;
+//add room name to DOM
+function outputRoomName(room){
+    roomName.innerText = room;
 }
 
-// Add users to DOM
-function outputUsers(users) {
-  userList.innerHTML = '';
-  users.forEach((user) => {
-    const li = document.createElement('li');
-    li.innerText = user.username;
-    userList.appendChild(li);
-  });
+//add users to DOM
+function outputUsers(users){
+    userList.innerHTML=`${users.map(user =>`<li>${user,username}</li>`).join("")}`;
 }
-
-//Prompt the user before leave chat room
-document.getElementById('leave-btn').addEventListener('click', () => {
-  const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
-  if (leaveRoom) {
-    window.location = '../index.html';
-  } else {
-  }
-});
